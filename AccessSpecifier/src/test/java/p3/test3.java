fa3e2a376d5c9bbf98ea1f7567f343acad3c6672
vishwagudi1807 <gudivishwanath218@gmail.com>
2025-05-02 12:32:36 +0530

    sample test4

---
+++ AccessSpecifier/src/test/java/p3/test3.java
@@ -0,0 +1,13 @@
+package p3;
+
+public class test3 {
+    public static void main(String[] args) {
+
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+
+
+    }
+}
