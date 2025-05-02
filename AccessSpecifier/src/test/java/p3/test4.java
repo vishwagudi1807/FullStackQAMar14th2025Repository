01e548be76b6357e11ad803336342052216b3e31
vishwagudi1807 <gudivishwanath218@gmail.com>
2025-05-02 12:39:19 +0530

    sample8

---
+++ AccessSpecifier/src/test/java/p3/test4.java
@@ -0,0 +1,20 @@
+package p3;
+class Even
+{
+    void displayeven(int a)
+    {
+        if( a%2==0)
+        {
+            System.out.println(" the given number is even :"+ a);
+        }
+        else
+            System.out.println(" its not an even");
+    }
+}
+public class test4 {
+    public static void main(String[] args) {
+        Even even =new Even();
+        even.displayeven(32);
+
+    }
+}
